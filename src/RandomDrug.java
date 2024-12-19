f1bf352e4c0ce676103f0ef693309110003bb8c5
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:56:03 +0200

    Update RandomDrug.java
    Remove comments

--- src/RandomDrug.java
+++ src/RandomDrug.java
@@ -2,12 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface RandomDrug {
-    public default Drug getRandomDrug() { //метод, который возвращает случайное лекарство
-        int index = (int) ((Math.random() * 1000) % (DrugsController.allDrugs.size())); //создаем переменную-контейнер, в
-        // которой будем хранить случайный(рандомный) индекс  целочисленном виде (это мы обеспечиваем за счет привидения типа к int)
-        //при остатке от деления числа(0 до 1000) на размер списка всех лекарств
-        List<Drug> drugs = new ArrayList<>(DrugsController.allDrugs.keySet()); //создаем список с типом данных <Drug> в который
-        // возвращаем набор всех ключей из карты всех лекарств
-        return drugs.get(index); //возвращаем лекарство по рандомному индексу из списка(List) лекарств
+    public default Drug getRandomDrug() {
+        int index = (int) ((Math.random() * 1000) % (DrugsController.allDrugs.size()));
+        
+        List<Drug> drugs = new ArrayList<>(DrugsController.allDrugs.keySet()); 
+        
+        return drugs.get(index);
     }
 }

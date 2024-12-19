92fc8588f12c05de1e2fdd08240d68f3da521297
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:55:31 +0200

    Update RandomCount.java
    Remove comments

--- src/RandomCount.java
+++ src/RandomCount.java
@@ -1,5 +1,5 @@
-public interface RandomCount { //интерфейс СлучайноеЧисло
-    public default int getRandomCount() { //метод для возврата случайного(рандомного) числа для покупки клиентом лекарства
-        return (int) (Math.random() * 5) + 1; //возвращаем число от 1 до 5
+public interface RandomCount {
+    public default int getRandomCount() {
+        return (int) (Math.random() * 5) + 1;
     }
 }
